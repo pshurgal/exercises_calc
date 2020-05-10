39effcfba39d8837eca1b498a3f579b39d83d178
Petr Shurgalin <petr.shurgalin@gmail.com>
2020-05-10 14:57:35 +0300

    Pages Initial Commit

---
+++ precache-manifest.8858114c5f3348068f5bb942232ada9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d70d1ef4b544abe9048ad17de2b0d933",
+    "url": "/exercises_calc/index.html"
+  },
+  {
+    "revision": "d867be1aaff0363282c7",
+    "url": "/exercises_calc/js/app.ae0383ac.js"
+  },
+  {
+    "revision": "40e97800b1f78203765e",
+    "url": "/exercises_calc/js/chunk-2d0cc1e7.be51d5d6.js"
+  },
+  {
+    "revision": "da13c24704349177d661",
+    "url": "/exercises_calc/js/chunk-2d0f001e.9c919252.js"
+  },
+  {
+    "revision": "0b33e2303e5781159c20",
+    "url": "/exercises_calc/js/chunk-2d2213d5.b6cdafaf.js"
+  },
+  {
+    "revision": "a60c9f597d0bef152807",
+    "url": "/exercises_calc/js/chunk-vendors.c4b075fb.js"
+  },
+  {
+    "revision": "2719d255bdf9b7a3d9a76a69738b657b",
+    "url": "/exercises_calc/manifest.json"
+  }
+]);
